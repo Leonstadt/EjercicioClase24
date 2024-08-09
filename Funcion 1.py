eed3935a9e08fc2141e2c74948796b8bff9b501a
Leonstadt <alfredo.ordonez@tecnologicoliceocristiano.edu.ec>
2024-08-08 22:28:48 -0500

    clase 24 participacion

---
+++ Funcion 1.py
@@ -0,0 +1,8 @@
+# Python Function with No Arguments, and No Return Value FUNCTION 1
+# Funcion de pyton sin argumentos y sin valor de retorno (1)
+def Add1():
+    a = 20
+    b = 30
+    Sum = a + b
+    print("After Calling the Function:", Sum)
+Add1()
